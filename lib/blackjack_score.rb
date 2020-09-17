c269010c521936197881d2bac0e5dee0d3923898
Ren Carothers <rencarothers@gmail.com>
2020-09-16 21:00:17 -0700

    took out unnecessary comments/spaces

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,4 +1,3 @@
-# blackjack_score.rb
 # GAME INFO
 # In the card BlackJack each card has a value.
 
@@ -52,16 +51,11 @@ def blackjack_score(hand)
         hand_score += element
       when 'Jack', 'Queen', 'King'
         hand_score += 10
-      when 'Ace', 1 # when there's an ace, wait to count ace until all other cards counted
+      when 'Ace', 1
         ace_count += 1
       else
         raise ArgumentError.new("Invalid card(s) entry: '#{element}'!")
       end
-    # if element is in VALID_CARDS, it is acceptable input, if it is not, return Arg err
-    # if element is 2-10, element_score = element and add element_score to hand_score
-    # if element is Jake-King, give each the values and add to hand_score
-    # if element is 1 or ace, if hand_score + 11 > 21 then try hand_score + 1 and if that works, add 1 to hand_score, otherwise return Arg err
-    # at the end of all the loops, look at hand_score. if its under 21, return it, otherwise Arg Err over 21?
     end
   end
 
@@ -84,37 +78,4 @@ def blackjack_score(hand)
   else
     return hand_score
   end
-  # Code for the score value of each possible card.  Note that Ace I guess equals 1?/ Jack 11, etc?
-  # If the array contains an invalid card value or the total exceeds 21, raise an ArgumentError.
-  # # You should also raise an error if the hand contains more than 5 cards.
 end
-
-
-# below this line = testing for tests -->
-# # CORRECT OUTPUT
-# test1_example = [3, 4] # => 7
-# p blackjack_score(test1_example)
-#
-# # # CORRECT OUTPUT
-# test2_example = ['Queen', 5, 2] # => 17
-# p blackjack_score(test2_example)
-#
-# # CORRECT OUTPUT
-# test3_example = ['Ace', 5, 2] # => 18
-# p blackjack_score(test3_example)
-
-# # CORRECT ERROR, gave over 21 error
-# test4_example = ['Ace', 5, 8] # => 14
-# p blackjack_score(test4_example)
-
-# CORRECT ERROR
-# test5_example = ['Ace', 'Banana', 'Jack'] # => ArgumentError for invalid cards
-# p blackjack_score(test5_example)
-
-# # CORRECT ERROR
-# test6_example = [8, 10, 8] # => ArgumentError for score over 21
-# p blackjack_score(test6_example)
-
-# CORRECT ERROR
-# test7_example = [2, 3, 4, 4, 2, 4] # => ArgumentError for more than 5 cards
-# p blackjack_score(test7_example)
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -7,43 +7,25 @@ require 'minitest/pride'
 require_relative '../lib/blackjack_score'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-# HEY YOU! when refactor, want to make sure I have nominal pos/neg and edge cases, friend
-
-# test1_example = [3, 4] # => 7
-# test2_example = ['Queen', 5, 2] # => 19
-# test3_example = ['Ace', 5, 2] # => 18
-# test4_example = ['Ace', 5, 8] # => 14
-# test5_example = ['Ace', 'Banana', 'Jack'] # => ArgumentError for invalid cards
-# test6_example = [8, 10, 8] # => ArgumentError for score over 21
-
 
 describe 'Blackjack Score' do
-  it 'can calculate the score for a pair of number cards' do
-    # Arrange
-    hand = [3, 4]
-    # Act
-    score = blackjack_score(hand)
-    # Assert
-    expect(score).must_equal 7
 
-    expect(blackjack_score([10, 10])).must_equal 20
+  it 'can calculate the score for a pair of number cards' do
+    hand = [3, 4]
+    score = blackjack_score(hand)
+    expect(score).must_equal 7
   end
 
   it 'calculates facecard values correctly' do
     hand = ['Queen', 5, 2]
     score = blackjack_score(hand)
     expect(score).must_equal 17
-
-    expect(blackjack_score(['Queen', 'Queen'])).must_equal 20
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
-    hand = ['Ace', 5, 2]
+    hand = ['Ace', 'Queen']
     score = blackjack_score(hand)
-    expect(score).must_equal 18
-
-    expect(blackjack_score(['Ace', 'Ace', 'Ace'])).must_equal 13
-    expect(blackjack_score(['Ace', 'Queen'])).must_equal 21 # EDGE CASE
+    expect(score).must_equal 21
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
@@ -61,21 +43,4 @@ describe 'Blackjack Score' do
     }.must_raise ArgumentError
   end
 
-  # it 'raises an ArgumentError for invalid cards' do
-  #   hand = ['Ace', 'Banana', 'Jack']
-  #   score = blackjack_score(hand)
-  #   expect(score).must_equal 18
-  # end
-  #
-  # it 'raises an ArgumentError for scores over 21' do
-  #   hand = [8, 10, 8]
-  #   score = blackjack_score(hand)
-  #   expect(score).must_equal 18
-  # end
-  #
-  # it 'raises an ArgumentError if a hand of more than 5 cards is passed' do # I added
-  #   hand = [2, 3, 4, 4, 2, 4]
-  #   score = blackjack_score(hand)
-  #   expect(score).must_equal 18
-  # end
 end
